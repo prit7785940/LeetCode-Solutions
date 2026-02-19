abc01970a081433c84b221044634557a858c9386
Priti Kumari <pk7785940@gmail.com>
2026-02-19 12:09:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.28%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long result = 1;
+        int N = m + n - 2;
+        int r = min(m - 1, n - 1);  
+        
+        for(int i = 1; i <= r; i++) {
+            result = result * (N - r + i) / i;
+        }
+        
+        return (int)result;
+    }
+};
