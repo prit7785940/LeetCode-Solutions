b22eeca0eecd383e1b5c88b08fba51e6ec1b25c5
Priti Kumari <pk7785940@gmail.com>
2026-05-27 20:26:25 +0530

    Time: 2 ms (76.96%) | Memory: 7.9 MB (38.27%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0){
+        if(n==0)
+        {
             return 0;
         }
         else if(n==1){
             return 1;
-
-        }
-        int ft = 0;
-        int st = 1;
-        for(int i=1;i<=n;i++){
-
-            int tt = ft + st;
-            ft = st;
-            st = tt;
-        }
-        return ft;
+        } 
+                int firstterm=0;
+                int secondterm=1;
+                for(int i=2;i<=n;i++){
+                int thirdterm=firstterm+secondterm;
+                firstterm=secondterm;
+                secondterm=thirdterm;
+            }
+           
+    return secondterm;
+    
     }
+
 };
